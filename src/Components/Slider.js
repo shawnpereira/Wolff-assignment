9227c47f5a490a06074256b2482893e5534b5dba
shawn <shawnpereiraswp@gmail.com>
2024-01-30 22:53:10 +0530

    cursor

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./SliderComponent.css"; // Import the CSS file
 
 const SliderComponent = () => {
   const settings = {
